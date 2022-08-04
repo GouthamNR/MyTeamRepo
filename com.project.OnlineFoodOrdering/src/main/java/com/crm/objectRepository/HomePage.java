762ca7a571b959d133b448d3918f27c5211db8b7
GouthamNR <gouthamnr5@gmail.com>
2022-08-04 17:35:07 +0530

    Homepare, loginPage, myOrderPage

--- com.project.OnlineFoodOrdering/src/main/java/com/crm/objectRepository/HomePage.java
+++ com.project.OnlineFoodOrdering/src/main/java/com/crm/objectRepository/HomePage.java
@@ -4,8 +4,16 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-
+/**
+ * 
+ * @author Goutham
+ *
+ */
 public class HomePage {
+	/**
+	 * 
+	 * @param driver
+	 */
 	public HomePage(WebDriver driver) {
 		PageFactory.initElements(driver, this);
 	}
--- com.project.OnlineFoodOrdering/src/main/java/com/crm/objectRepository/LoginPage.java
+++ com.project.OnlineFoodOrdering/src/main/java/com/crm/objectRepository/LoginPage.java
@@ -4,7 +4,11 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-
+/**
+ * 
+ * @author Goutham
+ *
+ */
 public class LoginPage {
 	
 	public LoginPage(WebDriver driver) {
